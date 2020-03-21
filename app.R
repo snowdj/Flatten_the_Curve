c4252fa9b0a0d598d75eeac1650b6111f86501db
Tinu <ti.s@bluewin.ch>
2020-03-21 17:47:43 +0100

    add R_0 to the Parameters
    Huge refactor ...

--- app.R
+++ app.R
@@ -14,7 +14,7 @@ options(theme_set(theme_minimal(base_size = 15)))
 # -----------------------------------------------------------
 #  t - time
 #  y - current state vector of the ODE at time t
-#  parms - Parameter vector used by the ODE system
+#  parms - Parameter vector used by the ODE system (beta, gamma)
 
 sir <- function(t, y, parms, 
                 social_dist_period, 
@@ -41,7 +41,7 @@ sir <- function(t, y, parms,
 
 
 ## we assume that many globals exist...!
-solve_ode <- function(sdp, red, typ) {
+solve_ode <- function(sdp, red, typ, beta) {
     ode_solution <- lsoda(y = c(N - n_init, n_init), 
                           times = times, 
                           func  = sir, 
@@ -74,37 +74,42 @@ N <- 1e6
 # Rate at which person stays in the infectious compartment (disease specific and tracing specific)
 gamma <- 1/5 
 
-# Infectious contact rate - beta = R0/N*gamma and when R0  ~2.25 then  2.25/N*gamma
-beta <- 4.5e-07 # CAUTION, depends on R0 which is defined below...
-
 # R0 for the beta and gamma values
-R0 <- beta*N/gamma
+# R0 <- beta*N/gamma
 
 # Initial number of infected people
 n_init <- 10
 
 # Grid where to evaluate
-max_time <- 150
-times <- seq(0, max_time, by = 0.01)
+max_time <- 365 # 150
+times <- seq(0, max_time, by = 0.1)
+
+# calculate beta
+# Infectious contact rate - beta = R0/N*gamma and when R0  ~2.25 then  2.25/N*gamma
+# beta <- 4.5e-07 
 
 
-## run only once
-if (!exists("ode_solution_daily")) {
+
+
+# add results with intervention and plot
+run <- function(sdp, red, r0) {
+    
+    beta <- r0 / N * gamma
+    
+    
     ode_solution_daily <- solve_ode(
         sdp = c(0, max_time),     # social_dist_period
         red = c(0.9999, 0.9999),  # reduction
-        typ = "without intervention"
+        typ = "without", 
+        beta = beta
     )
-}
-
-# add results with intervention and plot
-run <- function(sdp, red) {
     
     # solve with interventions
     ode_solution2_daily <- solve_ode(
         sdp = sdp,
         red = red,
-        typ = "with intervention"
+        typ = "with", 
+        beta = beta
     )
     
     # Combine the two solutions into one dataset
@@ -115,10 +120,18 @@ run <- function(sdp, red) {
             group_by(type) %>%
             filter(row_number() == n()) %>%
             mutate("final fraction" = scales::percent(1 - s, accuracy = 1)) %>%
-            select("final fraction", type) %>% 
-            arrange(desc(type))
+            select("final fraction", interventions = type) %>% 
+            arrange(desc(interventions))
     
     # Plot
+    y_max <- 0.09
+    y_arrow <- y_max * 0.95
+    y_text  <- y_arrow + 0.001
+    col_sdp <- "lightblue3"
+    
+    x_labs <- sort(c(0, 100, 200, 300, 365, sdp))
+    
+    
     pp <- ggplot(ode_df, 
                  aes(x = t, 
                      y = 0, 
@@ -131,68 +144,49 @@ run <- function(sdp, red) {
             x = "Days", 
             y = NULL, 
             subtitle = "Daily new cases in % of the population", 
-            caption  = "sdp: Start of social distance period\n
-                  red: Reduction of 'contacts per day' during period") +
-        scale_y_continuous(labels = scales::percent, 
-                           limits = c(0, 0.05)) +
-        theme(# axis.text.x = element_blank(),
-            # axis.text.y = element_blank(),
-            axis.ticks = element_blank(),
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank()) +
-        scale_color_brewer(name = "Epidemic model", 
+            caption  = "sdp: social distance period") +
+        scale_x_continuous(labels = x_labs, 
+                           breaks = x_labs) +
+        scale_y_continuous(labels = scales::percent,
+                           limits = c(0, y_max)) +
+        theme(axis.text.y = element_blank(),
+              axis.ticks  = element_blank(),
+              panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank()) +
+        scale_color_brewer(name = "Interventions", 
                            type = "qual", 
                            palette = 1, 
                            guide = guide_legend(reverse = TRUE)) +
-        # sdp text
+        # sdp 
         geom_vline(xintercept = sdp, lty = 2, color = "darkgray") +
-        geom_text(aes(x = sdp[1], 
-                      y = 0.05, 
+        geom_text(aes(x = sdp[1] + (sdp[2] - sdp[1])/2, 
+                      y = y_text, 
                       label = "sdp 1"),
                   vjust = 0,
-                  color = "blue2") +
-        geom_text(aes(x = sdp[2], 
-                      y = 0.05, 
+                  color = col_sdp) +   
+        geom_text(aes(x = sdp[2] + 40, 
+                      y = y_text, 
                       label = "sdp 2"),
                   vjust = 0,
-                  color = "blue2") +
-        # red text and arrows
-        geom_text(aes(x = sdp[1] + (sdp[2] - sdp[1])/2, 
-                      y = 0.048, 
-                      label = "red 1"),
-                  vjust = 0,
-                  color = "brown4") +   
-        geom_text(aes(x = sdp[2] + 20, 
-                      y = 0.048, 
-                      label = "red 2"),
-                  vjust = 0,
-                  color = "brown4") +   
+                  color = col_sdp) +   
         geom_segment(aes(
-            x = sdp[1],
-            y = 0.0475, 
-            xend = sdp[2] * 0.995,
-            yend = 0.0475
-        ),
-        size = 0.5, 
-        color = "brown4",
-        arrow = arrow(length = unit(2, "mm"))) +
+                x = sdp[1],
+                y = y_arrow, 
+                xend = sdp[2] * 0.99, # shorten
+                yend = y_arrow
+            ),
+            size = 0.3, 
+            color = col_sdp,
+            arrow = arrow(length = unit(2, "mm"))) +
         geom_segment(aes(
-            x = sdp[2]*1.01,
-            y = 0.0475, 
-            xend = max_time,
-            yend = 0.0475
-        ),
-        size = 0.5, 
-        color = "brown4",
-        arrow = arrow(length = unit(2, "mm")))  +
-        # capacity; removed
-        # geom_hline(yintercept = 0.02, lty = 3, color = "steelblue", size = 0.5) +
-        # geom_text(aes(x = 120, 
-        #               y = 0.021, 
-        #               label = "Healthcare system capacity"),
-        #           color ="steelblue", 
-        #           size = 3.5)
-        #
+                x = sdp[2]*1.01, # shorten
+                y = y_arrow, 
+                xend = max_time,
+                yend = y_arrow
+            ),
+            size = 0.3, 
+            color = col_sdp,
+            arrow = arrow(length = unit(2, "mm")))  +
         # Add final size as table
         annotation_custom(tableGrob(final_sizes, 
                                     rows = NULL,
@@ -200,10 +194,10 @@ run <- function(sdp, red) {
                                         core    = list(fg_params = list(hjust = 0, x = 0.1)),
                                         rowhead = list(fg_params = list(hjust = 0, x = 0))
                                     )),
-                          xmin = 100,
-                          xmax = 150,
-                          ymin = 0.037,
-                          ymax = 0.045
+                          xmin = max_time * 0.6,
+                          xmax = max_time,
+                          ymin = y_max * 0.7,
+                          ymax = y_max * 0.9
                           )
     
     print(pp)
@@ -212,35 +206,34 @@ run <- function(sdp, red) {
 
 ui <- fluidPage(
     
-    tags$head(tags$link(rel="shortcut icon", href="favicon.ico")),
-
     titlePanel("Flatten the Curve"),
     
     sidebarLayout(
         sidebarPanel(
-            # p("Change the reduction"),
-            sliderInput("sdp.one",
-                        "Start of first 'social distance period' (sdp 1)",
-                        min   =  0,
-                        max   = 50,
-                        value = 30, 
-                        step  =  5), 
+            sliderInput("r0", 
+                        div(HTML("Choose a value for R<sub>0</sub>")), 
+                        min   = 1.5, 
+                        max   = 3.0, 
+                        value = 2.25, 
+                        step  = 0.25),
+            hr(),
+            br(),
+            sliderInput("sdp",
+                        "Choose the first 'social distance period' (sdp 1)",
+                        min   =   0,
+                        max   = 150,
+                        value = c(30, 60), 
+                        step  =   5), 
+            br(), br(),
             sliderInput("red.one",
-                        "Reduction during first period (red 1)",
-                        min   = 0.4,
+                        "Reduction during first period (sdp 1)",
+                        min   = 0.2,
                         max   = 1.0,
                         value = 0.6, 
                         step  = 0.05), 
-            br(), br(),
-            sliderInput("sdp.two",
-                        "Start of second 'social distance period' (sdp 2)",
-                        min   =  50,
-                        max   = 100,
-                        value =  60, 
-                        step  =   5), 
             sliderInput("red.two",
-                        "Reduction during second period (red 2)",
-                        min   = 0.4,
+                        "Reduction after first period (sdp 2)",
+                        min   = 0.2,
                         max   = 1.0,
                         value = 0.8, 
                         step  = 0.05), 
@@ -260,14 +253,15 @@ ui <- fluidPage(
         ),
 
         mainPanel(
-           plotOutput("chart", height = "525px"), 
-           br(),
-           hr(),
-           h4("Initial code, mathematical model and idea:"),
-           tags$a(href="https://staff.math.su.se/hoehle/blog/2020/03/16/flatteningthecurve.html", 
-                  "Michael Höhle, 'Flatten the COVID-19 curve'"), 
-           p(),
-           br()
+            # p("Note: This tool is not intended to create a prediction."),
+            plotOutput("chart", height = "500px"), 
+            br(),
+            hr(),
+            h4("Initial code, mathematical model and idea:"),
+            tags$a(href="https://staff.math.su.se/hoehle/blog/2020/03/16/flatteningthecurve.html", 
+                   "Michael Höhle, 'Flatten the COVID-19 curve'"), 
+            p(),
+            br()
         )
     )
 )
@@ -276,8 +270,9 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$chart <- renderPlot({
-        run(sdp = c(input$sdp.one, input$sdp.two), 
-            red = c(input$red.one, input$red.two))
+        run(sdp = c(input$sdp), 
+            red = c(input$red.one, input$red.two), 
+            r0  = input$r0)
     })
 }
 
